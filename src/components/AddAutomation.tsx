5a74977670d4199d4dd8fa7cb3ac93e9c3792faa
Axel Padilla <adpadillar25@gmail.com>
2023-11-28 20:20:09 -0600

    add automation form

--- src/components/AddAutomation.tsx
+++ src/components/AddAutomation.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Switch } from "@/components/ui/switch";
 import {
   Dialog,
   DialogContent,
@@ -6,6 +7,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogClose,
 } from "@/components/ui/dialog";
 import {
   Select,
@@ -18,12 +20,14 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRefData } from "~/client/hooks/useRefData";
+import { type Action } from "~/client/utils/determineActions";
+import toast from "react-hot-toast";
 
 interface AddAutomationProps {
   children?: React.ReactNode;
 }
 
-export type Sensor = { name: string } & (
+export type Sensor = { name: string; triggers: string[] } & (
   | {
       type: "gas" | "temperature";
       triggers: ["gt", "lt"];
@@ -34,7 +38,7 @@ export type Sensor = { name: string } & (
     }
 );
 
-const getTriggerLabel = (trigger: string) => {
+export const getTriggerLabel = (trigger: string) => {
   switch (trigger) {
     case "gt":
       return "Mayor que";
@@ -59,8 +63,38 @@ const AddAutomation: React.FC<AddAutomationProps> = () => {
   const [actuator_db, actuator_db_loading] = useRefData<Actuator>("actuators");
   const [sensor_db, sensor_db_loading] = useRefData<Sensor>("sensors");
 
-  const [sensorId, setSensorId] = useState<string>();
-  const [actuatorId, setActuatorId] = useState<string>();
+  const [sensorId, setSensorId] = useState<string>("");
+  const [actuatorId, setActuatorId] = useState<string>("");
+  const [trigger, setTrigger] = useState<string>("");
+  const [newState, setNewState] = useState<boolean>(false);
+  const [parameter, setParameter] = useState<string>("");
+  const [stopAfter, setStopAfter] = useState<string>("");
+  const [revert, setRevert] = useState<boolean>(false);
+
+  const [selectedSensor, setSelectedSensor] = useState<
+    Sensor & { id: string }
+  >();
+  const [selectedActuator, setSelectedActuator] = useState<
+    Actuator & { id: string }
+  >();
+
+  const handleSubmit = () => {
+    const action: Action = {
+      actuatorId: selectedActuator?.id ?? "",
+      sensorId: selectedSensor?.id ?? "",
+      trigger: trigger as "gt" | "lt" | "on" | "off",
+      newState: {
+        enabled: newState,
+        metadata:
+          trigger === "gt" || trigger === "lt"
+            ? { value: parseInt(parameter) }
+            : undefined,
+        stopAfter: revert ? parseInt(stopAfter) : 0,
+      },
+    };
+
+    console.log(action);
+  };
 
   return (
     <div className="pt-4">
@@ -84,11 +118,16 @@ const AddAutomation: React.FC<AddAutomationProps> = () => {
 
             <div className="grid gap-y-4 py-8">
               <div className="flex w-full items-center justify-between space-x-4">
-                <p className="text-base font-semibold">Sensor</p>
+                <p className="text-base font-semibold">Cuando el sensor:</p>
                 <div>
                   <Select
                     value={sensorId}
-                    onValueChange={(v) => setSensorId(v)}
+                    onValueChange={(v) => {
+                      setSensorId(v);
+                      const sensor = sensor_db?.find((s) => s.id === v);
+                      setSelectedSensor(sensor);
+                      setTrigger(sensor?.triggers[0] ?? "");
+                    }}
                   >
                     <SelectTrigger className="w-[220px]">
                       <SelectValue placeholder="Selecciona un sensor" />
@@ -107,12 +146,59 @@ const AddAutomation: React.FC<AddAutomationProps> = () => {
                   </Select>
                 </div>
               </div>
+              {selectedSensor && (
+                <div className="flex w-full items-center justify-between space-x-4">
+                  <p className="text-base font-semibold">
+                    {selectedSensor.triggers.includes("on")
+                      ? "Cuando esta: "
+                      : "Cuando el valor es: "}
+                  </p>
+
+                  <div className="flex w-[220px] space-x-2">
+                    <Select
+                      value={trigger}
+                      onValueChange={(v) => {
+                        setTrigger(v);
+                      }}
+                    >
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Escoge..." />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {selectedSensor.triggers.map((trigger, idx) => (
+                          <SelectItem value={trigger} key={`${trigger}_${idx}`}>
+                            {getTriggerLabel(trigger)}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+
+                    {selectedSensor.triggers.includes("gt") && (
+                      <Input
+                        value={parameter}
+                        onChange={(e) => {
+                          const canParse = parseInt(e.target.value);
+                          if (isNaN(canParse)) {
+                            toast.error("El valor debe ser un número");
+                          }
+                          setParameter(e.target.value);
+                        }}
+                        className="w-full"
+                        placeholder="Valor"
+                      />
+                    )}
+                  </div>
+                </div>
+              )}
               <div className="flex w-full items-center justify-between space-x-4">
-                <p className="text-base font-semibold">Actuador</p>
+                <p className="text-base font-semibold">Cambiar actuador: </p>
                 <div>
                   <Select
                     value={actuatorId}
-                    onValueChange={(v) => setActuatorId(v)}
+                    onValueChange={(v) => {
+                      setActuatorId(v);
+                      setSelectedActuator(actuator_db?.find((a) => a.id === v));
+                    }}
                   >
                     <SelectTrigger className="w-[220px]">
                       <SelectValue placeholder="Selecciona un actuador" />
@@ -130,6 +216,84 @@ const AddAutomation: React.FC<AddAutomationProps> = () => {
                     </SelectContent>
                   </Select>
                 </div>
+              </div>
+              {selectedActuator && (
+                <div className="flex w-full items-center justify-between space-x-4">
+                  <p className="text-base font-semibold">A nuevo valor: </p>
+                  <div className="flex w-[220px] space-x-2">
+                    <Select
+                      value={newState ? "on" : "off"}
+                      onValueChange={(v) => {
+                        setNewState(v === "on");
+                      }}
+                    >
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Escoge..." />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {/* true or false, encendido y apagado */}
+                        <SelectItem value="on">Encendido</SelectItem>
+                        <SelectItem value="off">Apagado</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              )}
+              <div className="flex w-full items-center justify-between space-x-4">
+                <p className="text-base font-semibold">Activar duración:</p>
+                <div className="flex w-[220px] items-center space-x-8">
+                  <Switch
+                    checked={revert}
+                    onCheckedChange={(v) => setRevert(v)}
+                  />
+                  {revert && (
+                    <div className="flex items-center space-x-2">
+                      <Input
+                        value={stopAfter}
+                        onChange={(e) => {
+                          const canParse = parseInt(e.target.value);
+                          if (isNaN(canParse)) {
+                            toast.error("El valor debe ser un número");
+                          }
+                          setStopAfter(e.target.value);
+                        }}
+                        className="w-full"
+                        placeholder="Al pasar"
+                      />
+                      <p>Segs</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+              {false && (
+                <div className="flex w-full items-center justify-between space-x-4">
+                  <p className="text-base font-semibold">Detener después:</p>
+                  <div className="absolute flex w-[220px] items-center space-x-2">
+                    <Input
+                      value={parameter}
+                      onChange={(e) => {
+                        const canParse = parseInt(e.target.value);
+                        if (isNaN(canParse)) {
+                          toast.error("El valor debe ser un número");
+                        }
+                        setStopAfter(e.target.value);
+                      }}
+                      className="w-full"
+                      placeholder="Al pasar"
+                    />
+                    <p className="relative top-0">Segs</p>
+                  </div>
+                </div>
+              )}
+              <div className="flex w-full items-center space-x-4 pt-4">
+                <DialogClose>
+                  <Button variant="outline" asChild>
+                    <span>Cancelar</span>
+                  </Button>
+                </DialogClose>
+                <Button onClick={handleSubmit}>
+                  <span>Crear Automatización</span>
+                </Button>
               </div>
             </div>
           </DialogHeader>
