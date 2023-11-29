7bc0324979d23cae48613870e8628649da94e14b
NicoleArana <nicole.arana.sasj@gmail.com>
2023-11-28 22:16:52 -0600

    "la funcion que gepeto me ayudo a hacer"

--- src/client/utils/determineActions.ts
+++ src/client/utils/determineActions.ts
@@ -22,9 +22,69 @@ export type NewState = {
   enabled: boolean;
 };
 
+// export const determineActions = (
+//   actions: Array<Action>,
+//   measurements: Array<Measurement>,
+// ): Array<NewState> => {
+
+  
+//   return [];
+// };
+
 export const determineActions = (
   actions: Array<Action>,
   measurements: Array<Measurement>,
 ): Array<NewState> => {
-  return [];
+  const newStateArray: Array<NewState> = [];
+
+  actions.forEach((action) => {
+    const relevantMeasurement = measurements.find(
+      (measurement) => measurement.sensorId === action.sensorId
+    );
+
+    if (relevantMeasurement) {
+      switch (action.trigger) {
+        case "lt":
+          if (relevantMeasurement.value < action.newState.metadata.value) {
+            newStateArray.push({
+              actuatorId: action.actuatorId,
+              enabled: action.newState.enabled,
+            });
+          }
+          break;
+
+        case "gt":
+          if (relevantMeasurement.value > action.newState.metadata.value) {
+            newStateArray.push({
+              actuatorId: action.actuatorId,
+              enabled: action.newState.enabled,
+            });
+          }
+          break;
+
+        case "on":
+          if (relevantMeasurement.value === action.newState.metadata.value) {
+            newStateArray.push({
+              actuatorId: action.actuatorId,
+              enabled: action.newState.enabled,
+            });
+          }
+          break;
+
+        case "off":
+          if (relevantMeasurement.value !== action.newState.metadata.value) {
+            newStateArray.push({
+              actuatorId: action.actuatorId,
+              enabled: action.newState.enabled,
+            });
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+  });
+
+  return newStateArray;
 };
